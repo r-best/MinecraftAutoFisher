e7de819de601bcfc535b67e24b12eb4176deae6d
bobby <bobbyisbest3@gmail.com>
2018-10-23 11:55:30 -0400

    Improving timeout

--- test.py
+++ test.py
@@ -27,23 +27,26 @@ def main(argv):
     listener = keyboard.Listener(on_press=mouseDown)
     listener.start()
 
-    timeout = 45
+    timeout = 45 # Fish always appears within 45 seconds, if we wait longer than this then recast the line
+    cast_time = time.monotonic()
     while listener.is_alive():
         screen = ig.grab(bbox=(x1, y1, x2, y2))
         screen_text = pt.image_to_string(screen)
         print(screen_text)
         if 'Fishing Bobber splashes' in screen_text or 'Fishing Gobber splashes' in screen_text or 'Fickhina Bokhher splashes' in screen_text:
+            # Fish detected, clicking to reel it in & resetting timer
             pyautogui.click(WIDTH/2, HEIGHT/2)
             timeout = 45
-        if timeout <= 0:
-            timeout = 45
+        if time.monotonic() - cast_time > timeout:
+            # Timer has expired, must have missed the fish
             pyautogui.click(WIDTH/2, HEIGHT/2)
             time.sleep(0.25)
             pyautogui.click(WIDTH/2, HEIGHT/2)
+            timeout = 45
 
         time.sleep(1)
         timeout -= 1
-    
+
     listener.join()
 
 
