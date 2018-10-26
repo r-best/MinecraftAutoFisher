42cd07d54c9f9b0f3290985c4d08c2a470f6d064
bobby <bobbyisbest3@gmail.com>
2018-10-26 18:33:21 -0400

    Small adjustment to setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,21 @@ from setuptools import setup, find_packages
 with open('requirements.txt', 'r') as fp:
     reqs = [x.strip() for x in fp.readlines()]
 
-setup(name='Minecraft AutoFisher',
-      version='0.1',
-      description='',
-      long_description='',
-      classifiers=[
-        'Programming Language :: Python :: 3.6'
-      ],
-      keywords='',
-      url='https://github.com/r-best/MinecraftAutoFisher',
-      author='Robert Best',
-      author_email='',
-      license='',
-      packages=find_packages(),
-      install_requires=reqs,
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name='Minecraft AutoFisher',
+    version='0.1',
+    description='',
+    long_description='',
+    classifiers=[
+      'Programming Language :: Python :: 3.6'
+    ],
+    keywords='',
+    url='https://github.com/r-best/MinecraftAutoFisher',
+    author='Robert Best',
+    author_email='',
+    license='',
+    packages=find_packages(),
+    install_requires=reqs,
+    include_package_data=True,
+    zip_safe=False
+)
