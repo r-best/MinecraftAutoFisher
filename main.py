e3b5216685e5f50c58b8c98c46559857d6782276
bobby <bobbyisbest3@gmail.com>
2019-01-25 12:11:28 -0500

    Remove main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from autofisher.fisher import match
-# from autofisher.screengrabber import screengrabber
-
-
-# fisher.main(["config.json"])
-# screengrabber.main([])
-
-
-print(match("Fishing m", threshold=6))
