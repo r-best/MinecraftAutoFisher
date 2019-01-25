00c7c8c0bdd04d2578864ee179267d3fae200de1
bobby <bobbyisbest3@gmail.com>
2019-01-25 10:15:38 -0500

    Trying to mock pynput so readthedocs will build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,18 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
 
+if os.environ.get('READTHEDOCS'):
+    from unittest.mock import MagicMock
+
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+            return MagicMock()
+
+    MOCK_MODULES = ['pynput']
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
